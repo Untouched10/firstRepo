690beca2ba174c8f62d9a2d80eb3d9f871cd1691
Untouched10 <kulkarnivinay1010@gmail.com>
2024-01-13 15:12:33 +0530

    Added Address DropDown, Gender, TOB

--- resources/views/Student/student.blade.php
+++ resources/views/Student/student.blade.php
@@ -33,6 +33,36 @@
         </div>
 
         <div class="form-group">
+            <label for="state">State:</label>
+            <select class="form-control" id="state" name="state" required>
+                <!-- Add options for states -->
+                <option value="state1">State 1</option>
+                <option value="state2">State 2</option>
+                <!-- Add more options as needed -->
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="district">District:</label>
+            <select class="form-control" id="district" name="district" required>
+                <!-- Add options for districts -->
+                <option value="district1">District 1</option>
+                <option value="district2">District 2</option>
+                <!-- Add more options as needed -->
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="tehsil">Tehsil:</label>
+            <select class="form-control" id="tehsil" name="tehsil" required>
+                <!-- Add options for tehsils -->
+                <option value="tehsil1">Tehsil 1</option>
+                <option value="tehsil2">Tehsil 2</option>
+                <!-- Add more options as needed -->
+            </select>
+        </div>
+
+        <div class="form-group">
             <label for="name">Student email:</label>
             <input type="text" class="form-control" id="email" name="email" placeholder="Enter Student Email" required>
         </div>
@@ -43,8 +73,29 @@
         </div>
 
         <div class="form-group">
+            <label>Gender:</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="male" value="Male" required>
+                <label class="form-check-label" for="male">Male</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="female" value="Female">
+                <label class="form-check-label" for="female">Female</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="other" value="Other">
+                <label class="form-check-label" for="other">Other</label>
+            </div>
+        </div>
+
+        <div class="form-group">
             <label for="DOB">Student Date of Birth:</label>
             <input type="date" class="form-control" id="DOB" name="DOB" placeholder="Enter Date of Birth" required>
+        </div>
+
+        <div class="form-group">
+            <label for="TOB">Student Time of Birth:</label>
+            <input type="time" class="form-control" id="TOB" name="TOB" placeholder="Enter Time of Birth" required>
         </div>
 
         <div class="form-group">
